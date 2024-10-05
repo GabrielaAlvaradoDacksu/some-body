50c650259672a2544024bc82d8a09358c441f501
GabrielaAlvaradoDacksu <alvaradogabriela2603@gmail.com>
2024-10-05 17:51:31 -0400

    uggggh

--- script.js
+++ script.js
@@ -2,7 +2,18 @@
 let contenedor = document.getElementById('track-list')
 
 axios.get('https://api.institutoalfa.org/api/songs').then((response) => {
-    response.data.songs.map ( (song) => {
-        contenedor.innerHTML += song.title
+    response.data.songs.map((song) => {
+        // Ocurre por cada canción
+        let div = document.createElement('div')
+        div.setAttribute('class', 'flex p-4 gap-2')
+
+        div.innerHTML = `
+                <img src="https://api.institutoalfa.org/api/songs/image/${song.image.filename}">
+                    <div>
+                        <h3 class="font-bold">${song.title}</h3>
+                        <p class="opacity-40">${song.author}</p>
+                    </div>
+        `
+        contenedor.appendChild(div)
     })
 })
