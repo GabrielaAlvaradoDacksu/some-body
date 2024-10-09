b32a5349b1da4d3262a2bfcdbd260221b723826b
GabrielaAlvaradoDacksu <alvaradogabriela2603@gmail.com>
2024-10-08 21:31:22 -0400

    cooked

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ axios.get('https://api.institutoalfa.org/api/songs').then((response) => {
     response.data.songs.map((song) => {
         // Ocurre por cada canción
         let div = document.createElement('div')
-        div.setAttribute('class', 'flex p-4 gap-2')
+        div.setAttribute('class', 'track-music')
 
         div.innerHTML = `
-                <img src="https://api.institutoalfa.org/api/songs/image/${song.image.filename}">
+                <img src="https://api.institutoalfa.org/api/songs/image/${song.image.filename}" class="song-images"="">
                     <div>
-                        <h3 class="font-bold">${song.title}</h3>
-                        <p class="opacity-40">${song.author}</p>
+                        <h3 class="song-name">${song.title}</h3>
+                        <p class="artist">${song.author}</p>
                     </div>
         `
         contenedor.appendChild(div)
@@ -21,6 +21,6 @@ axios.get('https://api.institutoalfa.org/api/songs').then((response) => {
 let doof = document.querySelector('#doof');
     let sidebar = document.querySelector('#sidebar');
 
-    btn.onclick = function () {
+    doof.onclick = function () {
         sidebar.classList.toggle('active');
     };
